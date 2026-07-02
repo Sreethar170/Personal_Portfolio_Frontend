1849137f3016e21c05d5a45c08b7ee474e65b174
Sreethar170 <sreethar170@gmail.com>
2026-07-02 11:38:53 +0530

    latest local version

--- src/components/NavbarAchievements.js
+++ src/components/NavbarAchievements.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import './Navbar.css';
-import { Link, useLocation, useNavigate } from "react-router-dom";
- const NavbarAchievements =()=>{
-    return(
-      <>
-      <div className='nav'>
-       <nav className='navbar'>
-        <ul className='nav-link'>
-             <Link to="/" state={{ scrollTo: "" }} >Home</Link>
-              <Link to="/" state={{ scrollTo: "About" }} >About</Link>
-               <Link to="/" state={{ scrollTo: "Project" }} >Project</Link>
-                <Link to="/" state={{ scrollTo: "Experience" }} >Experience</Link>
-            <li className='Menu'><a href='/gallery'>Gallery</a></li>
-            <Link to="/" state={{ scrollTo: "About" }} >Contacts</Link>
-        </ul>  
-       </nav>
-       </div>
-       </>
-    );
- }
- export default NavbarAchievements;
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+const NavbarAchievements = () => {
+  return (
+    <>
+      <div className="nav">
+        <nav className="navbar">
+          <ul className="nav-link">
+            <li>
+              <Link to="/" state={{ scrollTo: "" }}>
+                Home
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" state={{ scrollTo: "About" }}>
+                About
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" state={{ scrollTo: "Project" }}>
+                Project
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/" state={{ scrollTo: "Experience" }}>
+                Experience
+              </Link>
+            </li>
+
+            <li className="Menu">
+              <Link to="/gallery">Gallery</Link>
+            </li>
+
+            <li>
+              <Link to="/" state={{ scrollTo: "Contacts" }}>
+                Contacts
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default NavbarAchievements;
