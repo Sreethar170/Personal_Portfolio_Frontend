f792627c356d59e9608fb497c55332280538ffa8
Sreethar170 <sreethar170@gmail.com>
2026-07-02 13:45:22 +0530

    latest local version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import NavbarAchievements from './components/NavbarAchievements';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Gallery from "./pages/Gallery";
@@ -11,7 +11,7 @@ import SecretPage from "./pages/SecretPage";
 function AppContent() {
   const location = useLocation();
 
-
+  const isGalleryPage = location.pathname.toLowerCase().includes("gallery");
 
   return (
     <>
